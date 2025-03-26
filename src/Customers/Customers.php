df4e3e58053452320d13582a9f25c6866a740dd0
Alisson Guedes <alissonguedes87@gmail.com>
2025-03-26 13:37:58 -0300

    Atualização Customers

--- src/Customers/Customers.php
+++ src/Customers/Customers.php
@@ -3,27 +3,41 @@ namespace AlissonGuedes\AsaasIntegration\Customers;
 
 use AlissonGuedes\AsaasIntegration\Asaas;
 
-class Customers {
+class Customers
+{
 
 	private $asaas;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->asaas = new Asaas;
 	}
 
-	public function get(array $data = [], int $offset = 0, int $limit = 10): array {
-		return Asaas::get('customers', $data);
+	public function get(string $cpfCnpj = null, int $offset = 0, int $limit = 10): array | bool
+	{
+		$get = Asaas::get('customers', ['cpfCnpj' => $cpfCnpj]);
+		if ($get['totalCount'] > 0) {
+			return $get['data'];
+		}
+		return false;
 	}
 
-	public function create(array $data): array {
-		return Asaas::create('customers', $data);
+	public function create(array $data): array | bool
+	{
+		$created = Asaas::create('customers', $data);
+		if ($created) {
+			return $created;
+		}
+		return false;
 	}
 
-	public function update(array $data): array {
+	public function update(array $data): array
+	{
 		return Asaas::update('customers', $data);
 	}
 
-	public function delete(array $data) {
+	public function delete(array $data)
+	{
 		return Asaas::delete('customers', $data);
 	}
 
